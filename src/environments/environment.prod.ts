616fafe180900d03e221e30fafdb606f83ce2417
ijquenta <ivanjosuequentavargas@gmail.com>
2024-04-15 18:49:41 -0400

    modify environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,13 @@
 
-export const API_URL = 'http://localhost:5001/academico_api';
+// export const API_URL = 'http://localhost:5001/academico_api';
+export const API_URL = 'https://proyectoibcibackend-production.up.railway.app/academico_api';
+
 export const API_URL2 = 'https://fake-trello-api.herokuapp.com';
 export const TOKEN = 'token';
 
 export const environment = {
     production: true,
-    API_URL: 'http://0.0.0.0:5001/academico_api'
+    // API_URL: 'http://0.0.0.0:5001/academico_api'
+    API_URL: 'https://proyectoibcibackend-production.up.railway.app/academico_api'
   };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 // export const API_URL = 'http://172.24.23.150:5001/academico_api';
-export const API_URL = 'http://localhost:5001/academico_api';
+export const API_URL = 'https://proyectoibcibackend-production.up.railway.app/academico_api';
+// export const API_URL = 'http://localhost:5001/academico_api';
+
 export const API_URL2 = 'https://fake-trello-api.herokuapp.com';
 export const TOKEN = 'token';
 
@@ -11,7 +13,9 @@ export const environment = {
   production: false,
   API_URL2: 'https://fake-trello-api.herokuapp.com',
 //   API_URL: 'http://172.24.23.150:5001/academico_api'
-  API_URL: 'http://0.0.0.0:5001/academico_api'
+//   API_URL: 'http://0.0.0.0:5001/academico_api'
+    API_URL: 'https://proyectoibcibackend-production.up.railway.app/academico_api'
+
 };
 
 /*
