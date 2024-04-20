c6684ab0f667b4441d60fc6492926824767c9283
ijquenta <ivanjosuequentavargas@gmail.com>
2024-04-19 20:25:02 -0400

    add apiUrl images

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,6 +9,6 @@ export const environment = {
     production: true,
     // API_URL: 'http://0.0.0.0:5001/academico_api',
     API_URL: 'https://proyectoibcibackend-production.up.railway.app/academico_api',
-    API_URL_FOTO_PERFIL: 'hhttps://proyectoibcibackend-production.up.railway.app/static/files_fotoperfil'
+    API_URL_FOTO_PERFIL: 'https://proyectoibcibackend-production.up.railway.app/static/files_fotoperfil'
   };
 
