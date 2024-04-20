ccb3b22523ab4870ab654ae538cf71b34e070f5d
ijquenta <ivanjosuequentavargas@gmail.com>
2024-04-19 20:04:49 -0400

    add env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+
+// export const API_URL = 'http://localhost:5001/academico_api';
+export const API_URL = 'https://proyectoibcibackend-production.up.railway.app/academico_api';
+
+export const API_URL2 = 'https://fake-trello-api.herokuapp.com';
+export const TOKEN = 'token';
+
+export const environment = {
+    production: true,
+    // API_URL: 'http://0.0.0.0:5001/academico_api',
+    API_URL: 'https://proyectoibcibackend-production.up.railway.app/academico_api',
+    API_URL_FOTO_PERFIL: 'hhttps://proyectoibcibackend-production.up.railway.app/static/files_fotoperfil'
+  };
+
