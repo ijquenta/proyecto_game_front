05ef1bab1911abf3f8a4f5cf983a2480abc39a08
ijquenta <ivanjosuequentavargas@gmail.com>
2024-04-24 00:21:40 -0400

    add paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { CursoModule } from './modules/components/curso/curso.module';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-// import { NotfoundComponent } from './modules/components/notfound/notfound.component';
+import { NotfoundComponent } from './modules/components/notfound/notfound.component';
 import { AppLayoutComponent } from "./modules/layout/app.layout.component";
 import { IsLoggedInGuard } from './guards/is-logged-in.guard';
 
@@ -25,7 +25,9 @@ const routes: Routes = [
     // loadChildren: () => import('./release/components/dashboard/dashboard.module').then((m) => m.DashboardModule),
     component: AppLayoutComponent,
                 children: [
-                    { path: '', loadChildren: () => import('./modules/components/principal/principal.module').then(m => m.PrincipaldModule), canActivate: [ AuthGuard ] },
+                    { path: '', loadChildren: () => import('./modules/components/panel/panel.module').then(m => m.PanelModule), canActivate: [ AuthGuard ] },
+                    { path: 'principal', loadChildren: () => import('./modules/components/panel/panel.module').then(m => m.PanelModule), canActivate: [ AuthGuard ] },
+                    { path: 'panel', loadChildren: () => import('./modules/components/principal/principal.module').then(m => m.PrincipaldModule), canActivate: [ AuthGuard ] },
                     // { path: '', loadChildren: () => import('./release/components/auth/login/login.module').then(m => m.LoginModule)},
                     // { path: 'uikit', loadChildren: () => import('./release/components/uikit/uikit.module').then(m => m.UIkitModule) },
                     // { path: 'utilities', loadChildren: () => import('./release/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
@@ -46,10 +48,21 @@ const routes: Routes = [
                     { path: 'matricula', loadChildren: () => import('./modules/components/matricula/matricula.module').then(m => m.MatriculaModule), canActivate: [ AuthGuard ]},
                     { path: 'nivel', loadChildren: () => import('./modules/components/nivel/nivel.module').then(m => m.NivelModule), canActivate: [ AuthGuard ]},
                     { path: 'curso', loadChildren: () => import('./modules/components/curso/curso.module').then(m => m.CursoModule), canActivate: [ AuthGuard ]},
-                    { path: 'mensaje', loadChildren: () => import('./modules/components/mensaje/mensaje.module').then(m => m.MensajeModule), canActivate: [ AuthGuard ]}
+                    { path: 'mensaje', loadChildren: () => import('./modules/components/mensaje/mensaje.module').then(m => m.MensajeModule), canActivate: [ AuthGuard ]},
+                    { path: 'contabilidad', loadChildren:() => import('./modules/components/contabilidad/contabilidad.module').then(m => m.ContabilidadModule), canActivate: [AuthGuard]},
+                    // { path: 'notfound', loadChildren:() => import('./modules/components/notfound/notfound.module').then(m => m.NotFoundModule), canActivate: [AuthGuard]},
+                     {
+                         path: 'notfound',
+                         component: NotfoundComponent
+                       },
+                      {
+                        path: '**',
+                        redirectTo: 'notfound'
+                      }
 
                     // { path: 'login', loadChildren:() => import('./release/components/auth/login/login.module').then(m => m.LoginModule)},
-                ]
+                ],
+
   },
 ];
 
