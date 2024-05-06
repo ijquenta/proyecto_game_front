7765017a85e8f7762db977f391732f90e228dffc
ijquenta <ivanjosuequentavargas@gmail.com>
2024-05-06 08:16:04 -0400

    add routes auth

--- src/app/modules/components/auth/auth-routing.module.ts
+++ src/app/modules/components/auth/auth-routing.module.ts
@@ -4,28 +4,45 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ForgotPasswordComponent } from './forgotPassword/forgot-password.component';
-
+import { ConfirmComponent } from './confirm/confirm.component';
+import { NoConfirmComponent } from './noConfirm/no-confirm.component';
+import { VerifiedComponent } from './verified/verified.component';
 
 const routes: Routes = [
     {
-      path: '',
-      redirectTo: 'login',
-      pathMatch: 'full'
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
     },
     {
-      path: 'login',
-      component: LoginComponent,
-      title: 'SGSA'
+        path: 'login',
+        component: LoginComponent,
+        title: 'SGSA'
     },
     {
-      path: 'register',
-      component: RegisterComponent,
-      title: 'SGSA'
+        path: 'register',
+        component: RegisterComponent,
+        title: 'SGSA'
     },
     {
-      path: 'forgot-password',
-      component: ForgotPasswordComponent,
-      title: 'SGSA'
+        path: 'forgot-password',
+        component: ForgotPasswordComponent,
+        title: 'SGSA'
+    },
+    {
+        path: 'confirm',
+        component: ConfirmComponent,
+        title: 'SGSA'
+    },
+    {
+        path: 'no-confirm',
+        component: NoConfirmComponent,
+        title: 'SGSA'
+    },
+    {
+        path: 'verified',
+        component: VerifiedComponent,
+        title: 'SGSA'
     },
     // {
     //   path: 'register',
