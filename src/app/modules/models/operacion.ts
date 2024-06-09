2057db7e115ae57dd49197c636c57864a652c9ba
ijquenta <ivanjosuequentavargas@gmail.com>
2024-06-09 02:04:25 -0400

    create operation model

---
+++ src/app/modules/models/operacion.ts
@@ -0,0 +1,10 @@
+export class Operacion {
+    opeid: number;
+    openombre?: string;
+    opeusureg?: string;
+    opefecreg?: Date;
+    opeusumod?: string;
+    opefecmod?: Date;
+    opedescripcion?: string;
+    opeestado: number;
+}
