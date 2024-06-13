112be5cf641316c90c32e49c04f758ef2ce1ff4e
ijquenta <ivanjosuequentavargas@gmail.com>
2024-06-13 19:10:36 -0400

    create submenu model

---
+++ src/app/modules/models/submenu.ts
@@ -0,0 +1,14 @@
+export class SubMenu {
+    submenid: number;
+    submennombre?: string;
+    menid?: number;
+    submenusureg?: string;
+    submenfecreg?: Date;
+    submenusumod?: string;
+    submenfecmod?: Date;
+    submendescripcion?: string;
+    submenestado: number;
+
+    // Extras
+    mennombre?: string;
+}
