ec2020ae1ba8136fcca7706aff84cb7ce517080a
ijquenta <ivanjosuequentavargas@gmail.com>
2024-06-17 16:18:32 -0400

    update upload files profile photo

--- src/app/modules/service/data/upload.service.ts
+++ src/app/modules/service/data/upload.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { checktoken } from 'src/app/interceptors/token.interceptor';
 import { API_URL } from 'src/environments/environment';
 
 @Injectable()
@@ -8,9 +9,28 @@ export class UploadService {
 
     constructor(private http: HttpClient) { }
 
-    uploadFilesFotoPerfil(criterio: any){
-        return this.http.post(`${API_URL}/fotoPerfil/upload`, criterio);
+    uploadProfilePhoto(criterio: any){
+        return this.http.post(`${API_URL}/profilePhoto/upload`, criterio, { context: checktoken()});
     }
+
+    deleteProfilePhoto(filename: string): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+          this.http.post(`${API_URL}/profilePhoto/delete`, { filename } , { context: checktoken() }).subscribe({
+            next: (data: any) => {
+              if (data.status === 'success') {
+                resolve(true);
+              } else {
+                resolve(false);
+              }
+            },
+            error: (error) => {
+              console.error('Error en la eliminación:', error);
+              resolve(false);
+            }
+          });
+        });
+    }
+
 
     uploadFilesPago(criterio: any){
         return this.http.post(`${API_URL}/pago/upload`, criterio);
