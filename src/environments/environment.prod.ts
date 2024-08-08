23221724c7a294ef788a9c79c5ad8e60fb5dc695
ijquenta <ivanjosuequentavargas@gmail.com>
2024-08-08 17:10:45 -0400

    add urls api backend railway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,10 +5,17 @@ export const API_URL = 'https://proyectoibcibackend-production.up.railway.app/ac
 export const API_URL2 = 'https://fake-trello-api.herokuapp.com';
 export const TOKEN = 'token';
 
+// Add
+export const API_URL_DOCUMENTO_ADMISION = 'https://proyectoibcibackend-production.up.railway.app/documentoAdmision';
+export const API_URL_PAGO_ARCHIVO = 'https://proyectoibcibackend-production.up.railway.app/static/files_pago';
 export const environment = {
     production: true,
     // API_URL: 'http://0.0.0.0:5001/academico_api',
     API_URL: 'https://proyectoibcibackend-production.up.railway.app/academico_api',
-    API_URL_PROFILE_PHOTO: 'https://proyectoibcibackend-production.up.railway.app/static/files_fotoperfil'
+    API_URL_PROFILE_PHOTO: 'https://proyectoibcibackend-production.up.railway.app/static/personProfilePhoto',
+
+    // Add
+    API_URL_DOCUMENTO_ADMISION: 'https://proyectoibcibackend-production.up.railway.app/static/documentoAdmision',
+    API_URL_PAGO_ARCHIVO: 'https://proyectoibcibackend-production.up.railway.app/static/files_pago'
   };
 
