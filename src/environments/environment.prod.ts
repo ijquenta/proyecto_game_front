a01f10dfe4ba41fa63c60e0a13a70cf69fbded32
ijquenta <ivanjosuequentavargas@gmail.com>
2024-08-19 12:27:36 -0400

    delete comments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,20 +1,11 @@
 
-// export const API_URL = 'http://localhost:5001/academico_api';
 export const API_URL = 'https://proyectoibcibackend-production.up.railway.app/academico_api';
-
-export const API_URL2 = 'https://fake-trello-api.herokuapp.com';
-export const TOKEN = 'token';
-
-// Add
 export const API_URL_DOCUMENTO_ADMISION = 'https://proyectoibcibackend-production.up.railway.app/documentoAdmision';
 export const API_URL_PAGO_ARCHIVO = 'https://proyectoibcibackend-production.up.railway.app/static/files_pago';
 export const environment = {
     production: true,
-    // API_URL: 'http://0.0.0.0:5001/academico_api',
     API_URL: 'https://proyectoibcibackend-production.up.railway.app/academico_api',
     API_URL_PROFILE_PHOTO: 'https://proyectoibcibackend-production.up.railway.app/static/personProfilePhoto',
-
-    // Add
     API_URL_DOCUMENTO_ADMISION: 'https://proyectoibcibackend-production.up.railway.app/static/documentoAdmision',
     API_URL_PAGO_ARCHIVO: 'https://proyectoibcibackend-production.up.railway.app/static/files_pago'
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,31 +2,17 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-// export const API_URL = 'http://172.24.23.150:5001/academico_api';
-// export const API_URL = 'https://proyectoibcibackend-production.up.railway.app/academico_api';
 export const API_URL = 'http://localhost:5001/academico_api';
 export const API_URL_PROFILE_PHOTO = 'http://localhost:5001/static/personProfilePhoto';
 export const API_URL_DOCUMENTO_ADMISION = 'http://localhost:5001/static/documentoAdmision';
-
-export const API_URL2 = 'https://fake-trello-api.herokuapp.com';
-export const TOKEN = 'token';
+export const API_URL_PAGO_ARCHIVO = 'http://192.168.0.41:5001/static/files_pago';
+export const API_URL_FILES_TEXTOS = 'http://0.0.0.0:5001/static/files_textos'
 
 export const environment = {
     production: false,
-    API_URL2: 'https://fake-trello-api.herokuapp.com',
-    //   API_URL: 'http://172.24.23.150:5001/academico_api'
-    API_URL: 'http://0.0.0.0:5001/academico_api',
-    // API_URL: 'https://proyectoibcibackend-production.up.railway.app/academico_api',
+    API_URL: 'http://localhost:5001/academico_api',
     API_URL_PROFILE_PHOTO: 'http://0.0.0.0:5001/static/personProfilePhoto',
-    API_URL_DOCUMENTO_ADMISION: 'http://0.0.0.0:5001/static/documentoAdmision'
-
+    API_URL_DOCUMENTO_ADMISION: 'http://0.0.0.0:5001/static/documentoAdmision',
+    API_URL_PAGO_ARCHIVO: 'http://0.0.0.0:5001/static/files_pago',
+    API_URL_FILES_TEXTOS: 'http://0.0.0.0:5001/static/files_textos'
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
