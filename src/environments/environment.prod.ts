d66991e57244360be885bb8b4570d829fc571375
ijquenta <ivanjosuequentavargas@gmail.com>
2024-08-21 12:03:39 -0400

    add api_url_files_textos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,14 @@
 export const API_URL = 'https://proyectoibcibackend-production.up.railway.app/academico_api';
 export const API_URL_DOCUMENTO_ADMISION = 'https://proyectoibcibackend-production.up.railway.app/documentoAdmision';
 export const API_URL_PAGO_ARCHIVO = 'https://proyectoibcibackend-production.up.railway.app/static/files_pago';
+export const API_URL_FILES_TEXTOS = 'https://proyectoibcibackend-production.up.railway.app/static/files_textos'
+
 export const environment = {
     production: true,
     API_URL: 'https://proyectoibcibackend-production.up.railway.app/academico_api',
     API_URL_PROFILE_PHOTO: 'https://proyectoibcibackend-production.up.railway.app/static/personProfilePhoto',
     API_URL_DOCUMENTO_ADMISION: 'https://proyectoibcibackend-production.up.railway.app/static/documentoAdmision',
-    API_URL_PAGO_ARCHIVO: 'https://proyectoibcibackend-production.up.railway.app/static/files_pago'
+    API_URL_PAGO_ARCHIVO: 'https://proyectoibcibackend-production.up.railway.app/static/files_pago',
+    API_URL_FILES_TEXTOS:'https://proyectoibcibackend-production.up.railway.app/static/files_textos'
   };
 
